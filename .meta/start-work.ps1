f791cdc6fc20aa62c882d141c72a71d975f66aae
Phillix <p.f.zeratul@gmail.com>
2026-09-18 12:20:38 +0800

    update start-work

--- .meta/start-work.ps1
+++ .meta/start-work.ps1
@@ -7,3 +7,6 @@ Start-Process "warp://tab_config/life_rpg_tab_config"
 $exePath = "$env:LOCALAPPDATA\Programs\antigravity\Antigravity.exe"
 $workDir = "$env:LOCALAPPDATA\Programs\antigravity"
 Start-Process -FilePath $exePath -WorkingDirectory $workDir
+
+$githubDesktopPath = "$env:LOCALAPPDATA\GitHubDesktop\GitHubDesktop.exe"
+Start-Process -FilePath $githubDesktopPath -ArgumentList $rootFolder
