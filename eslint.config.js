b94913b25b137cfebbd8a009d39814bd963782eb
Phillix <p.f.zeratul@gmail.com>
2026-09-19 11:13:33 +0800

    Added Eslint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{ts, tsx}'],
+        languageOptions: {
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: ['eslint.config.js'],
+                },
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-floating-promises': 'error',
+        },
+    },
+    {
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+            'src-tauri/**'
+        ],
+    }
+);
